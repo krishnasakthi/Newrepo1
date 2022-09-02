0907226b82d8287ecdb3acd0349e7865fbdd64ba
krishna <krishna@DESKTOP-T51J9PB>
2022-09-02 08:19:48 +0530

    Create if condition

---
+++ IF_CONTROL/src/First_if.java
@@ -0,0 +1,20 @@
+
+public class First_if {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		
+		if(a <=20)
+		{
+			System.out.println("Then condition true exctu if block");
+		}
+		else
+		{
+			System.out.println("Then condition false exctu else block");
+		}
+
+	}
+
+}
